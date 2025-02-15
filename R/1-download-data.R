42f92d81c9bc6f84ea8fa376a50fb183cda8862d
AQLT <alain.quartier@yahoo.fr>
2025-02-15 21:25:21 +0100

    download update

--- R/1-download-data.R
+++ R/1-download-data.R
@@ -3,7 +3,8 @@ for (spec in list.files("data", pattern = "yml$",full.names = TRUE)){
 	data <- yaml::read_yaml(spec)
 	if (!dir.exists(file.path("data", data$dataset)))
 		dir.create(file.path("data", data$dataset))
-	data_insee <- AQLTools::lectureBDM(data$series)
+
+	data_insee <- AQLTools::lectureBDM(sapply(data$series, `[[`, "idbank"))
 	colnames(data_insee) <- names(data$series)
 	last_date <- format(zoo::as.Date(time(data_insee)), "%Y_%m")
 	last_date <- last_date[length(last_date)]
@@ -12,5 +13,7 @@ for (spec in list.files("data", pattern = "yml$",full.names = TRUE)){
 		data$dataset,
 		last_date
 	)
+	if (file.exists(file))
+		next;
 	write.ts(data_insee, file)
 }
